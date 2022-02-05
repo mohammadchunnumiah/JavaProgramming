3219fdac16db0e19f8488f8c58d7747f54649a51
miah <mcmiahus@gmail.com>
2022-02-05 00:02:40 -0500

    New test added

---
+++ src/main/java/javaProgrammingQuestions_SDET/P19_DuplicateElementsInArray_HashSet.java
@@ -0,0 +1,28 @@
+package javaProgrammingQuestions_SDET;
+
+import java.util.HashSet;
+
+public class P19_DuplicateElementsInArray_HashSet {
+
+	public static void main(String[] args) {
+
+		String arr[] = { "java", "C", "C++", "Python", "java" };
+
+		HashSet<String> langs = new HashSet();
+
+		boolean flag = false;
+
+		for (String l : arr) 
+		{
+			if (langs.add(l) == false) 
+			{
+				System.out.println("Found duplicate element:" + l);
+				flag = true;
+			}
+		}
+		if (flag = false) {
+			System.out.println("Not found duplicate element:");
+		}
+
+	}
+}
